362909dc23e4e0aeeb9de2e0fb525570cd56913b
Mathew Reynolds <mjjreynolds3200@gmail.com>
2022-02-08 17:29:05 +0000

    feat: add UUID to user

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Traits\UUID;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -10,7 +11,7 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, UUID, HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
